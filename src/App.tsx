923a7d6d2172eb09c387bbcf890955bb0bae7055
jayoung.FE <68357066+jayoung-cinnamon@users.noreply.github.com>
2021-11-12 17:35:34 +0900

    Delete File

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
 import React from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>First Commit!</p>
-      </header>
-    </div>
-  );
+  return <div className="App">hi</div>;
 }
 
 export default App;
