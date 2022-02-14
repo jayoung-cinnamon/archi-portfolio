001509a9894c9737abfc6bea9044cacd767b49cc
jayoung.FE <68357066+jayoung-cinnamon@users.noreply.github.com>
2022-02-14 17:41:26 +0900

    modify main

--- src/components/main/MainPage.tsx
+++ src/components/main/MainPage.tsx
@@ -19,13 +19,13 @@ const MainPage = () => {
   }, []);
   const { start, stop } = useInterval(
     () => {
-      if (titleIndex < 3) {
+      if (titleIndex < 4) {
         setTitleIndex(titleIndex + 1);
       } else {
         setTitleIndex(1);
       }
     },
-    1200,
+    2000,
     {
       autoStart: true,
       immediate: true,
@@ -34,12 +34,15 @@ const MainPage = () => {
   useEffect(() => {
     switch (titleIndex) {
       case 1:
-        setTitle("Architect");
+        setTitle("Wake & Remake");
         break;
       case 2:
-        setTitle("Portfolio");
+        setTitle("Architect");
         break;
       case 3:
+        setTitle("Portfolio");
+        break;
+      case 4:
         setTitle("Design");
         break;
       default:
@@ -50,12 +53,19 @@ const MainPage = () => {
   return (
     <Wrapper>
       <Body>
-        <TextContainer>{title}</TextContainer>
+        <TextContainer>
+          <TiltText>
+            {title}
+            <Bar className={`titleIndex${titleIndex}`}></Bar>
+          </TiltText>
+        </TextContainer>{" "}
         <ImageContainer>
           <MainImage />
         </ImageContainer>
         <AboutBtn>
-          <a href="/about">About</a>
+          <a className="button" href="/about">
+            About
+          </a>
         </AboutBtn>
       </Body>
     </Wrapper>
@@ -63,25 +73,6 @@ const MainPage = () => {
 };
 
 export default MainPage;
-
-const Cursor = styled.div`
-  .cursor {
-    height: 2rem;
-    width: 2px;
-    margin-left: 2px;
-    background: $main;
-    animation: blinkTextCursor 800ms infinite;
-  }
-
-  @keyframes blinkTextCursor {
-    from {
-      opacity: 1;
-    }
-    to {
-      opacity: 0;
-    }
-  }
-`;
 
 const Wrapper = styled.div`
   width: 100%;
@@ -100,61 +91,76 @@ const Body = styled.div`
   color: black;
   padding: 20px;
   display: flex;
-  position: relative;
-`;
-
-const MainTitle = styled.div`
-  font-weight: 800;
-  color: black;
-  top: 0px;
-  left: 100px;
-
-  cursor: pointer;
-  .typeContainer {
-    position: relative;
-    font-size: 1.8rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-`;
-
-const SubText = styled.div`
-  margin-top: 30px;
-  display: flex;
-  font-size: 16px;
-  color: grey;
-  width: 500px;
-  line-height: 1.8;
 `;
 
 const TiltText = styled.div`
-  font-size: 50px;
+  width: 450px;
+  font-size: 45px;
   font-weight: 900;
   font-style: oblique;
-  &:hover {
-    color: yellow;
-    cursor: pointer;
-    transition: ease-in 0.5s;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 15px;
+  /* width: 0; */
+  position: relative;
+
+  /* left: 0; */
+  z-index: 9999;
+`;
+
+const Bar = styled.div`
+  /* width: 500px; */
+  height: 60px;
+  display: flex;
+  align-items: center;
+  position: absolute;
+  padding: 5px;
+  top: 0;
+  left: 0;
+  z-index: -1;
+
+  border: 1px solid #cfcfcf44;
+  background-color: white;
+  color: black;
+  &.titleIndex1 {
+    animation: fill 2s infinite;
+  }
+  &.titleIndex2 {
+    animation: fill 2s infinite;
+  }
+  &.titleIndex3 {
+    animation: fill 2s infinite;
+  }
+  &.titleIndex4 {
+    animation: fill 2s infinite;
+  }
+
+  @keyframes fill {
+    100% {
+      background: linear-gradient(to left, #ffffff75);
+      width: 0%;
+    }
+    /* 50% {
+      background: linear-gradient(to left, #c2c2c2, #cfcfcf44);
+      width: 90%;
+    } */
+    0% {
+      background: linear-gradient(to left, #ffffff7e);
+      width: 100%;
+    }
   }
 `;
 
 const TextContainer = styled.div`
-  width: 500px;
-  height: 60px;
   position: absolute;
   top: 350px;
-  left: 400px;
-  border: 1px solid black;
-  /* left: 500px;
-  top: 350px; */
+  right: 450px;
+  /* border: 1px solid black; */
   z-index: 9999;
-  background-color: white;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 48px;
-  padding-top: 5px;
+  /* background-color: white; */
+
+  cursor: pointer;
 `;
 
 const ImageContainer = styled.div`
@@ -163,6 +169,7 @@ const ImageContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  position: relative;
 `;
 
 const MainImage = styled.div`
@@ -178,15 +185,54 @@ const MainImage = styled.div`
 `;
 
 const AboutBtn = styled.div`
-  width: 130px;
-  height: 40px;
   display: flex;
   justify-content: space-around;
   align-items: center;
-  border: 1px solid #ff6400;
   z-index: 999;
   position: absolute;
-  bottom: 20px;
-  right: 0;
+  bottom: 40px;
+  right: 200px;
   color: #209fac;
+  a {
+    text-decoration: none;
+    color: #616161;
+  }
+
+  .button {
+    min-width: 100px;
+    padding: 10px 8px;
+    text-align: center;
+    font-size: 15px;
+    /* background: #209eac40; */
+    color: #5e5e5e;
+    border: 1px solid #5e5e5e;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    position: relative;
+    transition: all 0.3s;
+  }
+
+  .button:hover {
+    background: white;
+    color: black;
+    padding-right: 35px;
+  }
+
+  .button::after {
+    content: "";
+    position: absolute;
+    width: 14px;
+    height: 13px;
+    right: 8px;
+    top: 10px;
+    background: url(https://image.flaticon.com/icons/png/512/109/109617.png)
+      no-repeat center/cover;
+    opacity: 0;
+    transition: opacity 0.5s, right 0.3s;
+  }
+
+  .button:hover::after {
+    opacity: 1;
+    right: 20px;
+  }
 `;
